d2f1b6518e6a6e928de68f59f96e883b59913e2b
Sam Wronski <runewake2@outlook.com>
2017-01-10 00:09:40 -0600

    Updated application to have constantly changing background color.

--- BinaryClock/MainPage.xaml.cs
+++ BinaryClock/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using BinaryClock.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,34 @@ namespace BinaryClock
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        MainViewModel viewModel = new MainViewModel();
+        Task colorUpdateTask;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.DataContext = viewModel;
+            colorUpdateTask = BeginColorUpdate(viewModel);
+        }
+
+        private Task BeginColorUpdate(MainViewModel viewModel)
+        {
+            return Task.Run(async () =>
+            {
+                int iterations = 0;
+                while (true)
+                {
+                    await Task.Delay(50);
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        viewModel.BackgroundColor = Color.FromArgb(255,
+                            (byte)(128 + (int)(Math.Sin(iterations * .01f) * 127)),
+                            (byte)(128 + (int)(Math.Sin(iterations * .0125f + 1) * 127)),
+                            (byte)(128 + (int)(Math.Sin(iterations * .009f + 1) * 127)));
+                    });
+                    iterations++;
+                }
+            });
         }
     }
 }
---
+++ BinaryClock/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace BinaryClock.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Color backgroundColor = Color.FromArgb(255, 0, 0, 255);
+
+        public Color BackgroundColor {
+            get { return backgroundColor; }
+            set {
+                if (backgroundColor != value)
+                {
+                    backgroundColor = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BackgroundColor)));
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BackgroundBrush)));
+                    }
+                }
+            }
+        }
+
+        public SolidColorBrush BackgroundBrush
+        {
+            get {
+                return new SolidColorBrush(BackgroundColor);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
