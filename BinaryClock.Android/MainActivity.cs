7d949d06a4a6bdb664cb9e71d67d769db3f33494
Sam Wronski <runewake2@outlook.com>
2017-03-22 23:07:56 -0500

    Introduce Android version of Binary Clock app. Currently just a test Xamarin application to get the feet wet.

---
+++ BinaryClock.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BinaryClock.Android
+{
+    [Activity(Label = "BinaryClock.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += (o,e) => sayHello();
+        }
+
+        public void sayHello()
+        {
+            var playerName = FindViewById<EditText>(Resource.Id.playerName);
+            var playerGreeting = FindViewById<TextView>(Resource.Id.playerGreeting);
+            playerGreeting.Text = "Hello, " + playerName.Text;
+        }
+    }
+}
+
